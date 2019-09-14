12a8e2583f7d29cdd3de573c89e42a6b6545cbf2
abhishar07 <46322271+abhishar07@users.noreply.github.com>
2019-09-14 20:58:05 +0530

    jdbc connection

---
+++ src/com/faceprep/jfs/JDBC.java
@@ -0,0 +1,46 @@
+package com.faceprep.jfs;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");  //searching for jdbc driver class
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jobportal", "root", "bhidoni2812#"); //getting connection with msql database
+			
+			if(conn != null)
+			{
+				System.out.println("connection established sucessfully");
+			}
+			else
+			{
+				System.out.println("Not connected");
+			}
+			
+		}
+		 catch(ClassNotFoundException e) {
+			   System.out.println("Driver class is not Found! Exception occured");
+		   }
+		   catch(SQLException se) {
+			   System.out.println(" Exception occured while creating a connection!!");
+			   System.out.println(se);
+		   }
+		   finally {
+			        try
+			       {
+				       conn.close();
+			       }
+			       catch(SQLException sqlE) {
+				       System.out.println("Exception occured while closing a connection");
+			        }
+			    }
+
+	}
+
+}
